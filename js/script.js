9331209625ab77a00326cc7d876f89c89f91d63e
Dan Hooyboer <dan.hooyboer@fusionmedstaff.com>
2023-02-28 09:46:27 -0600

    Remove unused function Function has been moved to separate util.js file and was commented out.

--- js/script.js
+++ js/script.js
@@ -11,21 +11,6 @@ RockButton.addEventListener('click', function() {playRound("rock")});
 PaperButton.addEventListener('click', function() {playRound("paper")});
 ScissorsButton.addEventListener('click', function() {playRound("scissors")});
 
-/* function getComputerChoice() {
-  let randomInt = Math.floor(Math.random() * 3);
-  let computerChoice = ""
-  if (randomInt === 0) {
-    computerChoice = "rock";
-  } else if (randomInt === 1) {
-    computerChoice = "paper";
-  } else if (randomInt === 2) {
-    computerChoice = "scissors";
-  } else {
-    console.log("Something has gone wrong with getComputerChoice.")
-  }
-  return computerChoice;
-} */
-
 function playRound(playerChoice) {
   if (gameOver) {return};
   let winner = ""
